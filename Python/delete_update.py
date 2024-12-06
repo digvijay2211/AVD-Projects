b551ee2e9d8455e0fa6c5160b737d61c21982e7c
Rsarag96 <rupeshsarag267@gmail.com>
2024-12-06 12:26:14 +0530

    final update added

--- Python/delete_update.py
+++ Python/delete_update.py
@@ -1,28 +1,27 @@
 def delete_rows(cursor, conn):
 
-    id = int(input('enter no: '))
-    str = "delete from pra where id ='%d'"  # execute this SQL command at db server
-    args = id
+    tbname = input("enter the table name: ")
+    tbid = int(input("enter id no: "))
+    str1 = f"delete from {tbname} where id = '{tbid}'"  # execute this SQL command at db server
 
-    cursor.execute(str %args)
-
+    cursor.execute(str1)
     conn.commit()
+    print('row is deleted')
 
     cursor.close()
-    print('1 row is deleted')
 
 #update rows
 def update_rows(cursor, conn):
+    tbname = input("enter the table name: ")
     id = int(input('enter no: '))
     col_name =input("enter column name: ")
     col_value = input("enter value: ")
 
-    str = f"update pra set {col_name}='{col_value}' where id ={id}"  # execute this SQL command at db server
-
-    print(str)
-    cursor.execute(str)
+    str2 = f"update {tbname} set {col_name} ='{col_value}' where id = '{id}'"  # execute this SQL command at db server
+    cursor.execute(str2)
     conn.commit()
-    print('1 row is updated')
+    print(' row is updated')
 
+    cursor.close()
 
 
